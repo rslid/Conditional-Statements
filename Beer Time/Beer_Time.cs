38d464f77abdc123c09297609d51ffdc4a98bbe3
rslid <takepack@gmail.com>
2014-03-28 00:42:53 +0200

    homework

--- Beer Time/Beer_Time.cs
+++ Beer Time/Beer_Time.cs
@@ -6,18 +6,24 @@ class Beer_Time
 
     static void Main()
     {
-       
-        Console.WriteLine("Please enter a time (ex. 1:00 pm) = ");
-        var currentTime = Console.ReadLine();
-        DateTime now = Convert.ToDateTime(currentTime);
-        var startTime = new DateTime(now.Year, now.Month, now.Day, 13, 0, 0);
-        var endTime = new DateTime(now.Year, now.Month, now.Day, 3, 0, 0);
-        bool result = now.IsBetween(startTime, endTime);
-        if (result == true)
-            Console.WriteLine("beer time");
-        else
+        try
         {
-            Console.WriteLine("non-beer time");
+            Console.WriteLine("Please enter a time (ex. 1:00 pm) = ");
+            var currentTime = Console.ReadLine();
+            DateTime now = Convert.ToDateTime(currentTime);
+            var startTime = new DateTime(now.Year, now.Month, now.Day, 13, 0, 0);
+            var endTime = new DateTime(now.Year, now.Month, now.Day, 3, 0, 0);
+            bool result = now.IsBetween(startTime, endTime);
+            if (result == true)
+                Console.WriteLine("beer time");
+            else
+            {
+                Console.WriteLine("non-beer time");
+            }
+        }
+        catch (Exception)
+        {
+            Console.WriteLine("Invalid input");
         }
     }
 
